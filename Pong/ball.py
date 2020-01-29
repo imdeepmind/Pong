fa103a2bd44e2fb8081cbc7cf5e0998794dfaf5e
Abhishek Chatterjee - imdeepmind <abhishek.chatterjee97@protonemail.com>
2020-01-29 21:10:48 +0530

    added ball movement

--- Pong/ball.py
+++ Pong/ball.py
@@ -6,10 +6,21 @@ class Ball:
     def __init__(self):
         self.x = SCREEN_WIDTH // 2
         self.y = SCREEN_HEIGHT // 2
+        
         self.radius = 5
+
+        self.xv = 5
+        self.yv = 5
 
     def draw(self, screen):
         pygame.draw.circle(screen, RED, (self.x, self.y), self.radius)
     
     def update(self):
-        pass
+        if self.x >= SCREEN_WIDTH or self.x <= 0:
+            self.xv *= -1
+        
+        if self.y >= SCREEN_HEIGHT or self.y <= 0:
+          self.yv *= -1  
+
+        self.x += self.xv
+        self.y += self.yv
